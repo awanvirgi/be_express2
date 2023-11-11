5f7a7c55bfca63d81f6042cec9e98cbde4a496ac
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-12 03:53:43 +0700

    Initial Commit

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "freedb_virgiawan2",
+    "password": "k?8&5GV%bBZGEX8",
+    "database": "freedb_mytodos2",
+    "host": "sql.freedb.tech",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
